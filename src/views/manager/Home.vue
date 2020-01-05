cd857dfd8b87f24d71f38906f5f5edaf51f56b6f
LSR1123 <939268457@qq.com>
2020-01-05 10:55:56 +0800

    小的修改

--- src/views/manager/Home.vue
+++ src/views/manager/Home.vue
@@ -3,18 +3,61 @@
     <header class="header">
       <img src="../../assets/home.jpg" alt="">
     </header>
-   
+  <div>
+
+    <!-- 分类6个 -->
+  <van-grid :column-num="3">
+  <van-grid-item
+    v-for="value in categories"
+    :key="value.id"
+    :icon="value.icon"
+    :text="value.name"
+  />
+  </van-grid>
+    <!-- 产品 6个 -->
+  <van-grid :column-num="1" icon-size="400px">
+  <van-grid-item
+    v-for="value in products"
+    :key="value.id"
+    :icon="value.icon"
+    :text="value.name"
+  />
+</van-grid>
+  </div>
   </div>
 </template>
 <script>
+import {get,post} from '../../http/axios';
 import {mapState, mapActions} from 'vuex'
-export default {
-  
+export default { 
+  data(){
+    return{
+      categories:{},
+      products:{}
+    }
+  },
   created(){
+    this.LoadCategory();
+    this.loadProduct();
   },
   methods:{
-
-  }
+    LoadCategory(){
+      let url = "/category/findAll";
+      get(url).then((response)=>{
+        this.categories = response.data.slice(0,6);
+      })
+    }
+  },
+  loadProduct(){
+    let url = "/product/query"
+    let param = {
+      page : 0,
+      pageSize :10
+    }
+    post(url,params).then ((response)=>{
+      this.product = response.data.list;
+    })
+  },
 }
 </script>
 <style scoped>
